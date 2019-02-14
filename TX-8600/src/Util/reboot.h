3c56dcc3c861b5cd8cad9f0e81fd0fa58f03d8ae
ljh <1178443819@qq.com>
2019-02-14 09:45:26 +0800

    2月14克隆库新建分支

--- TX-8600/src/Util/reboot.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2013-2016, XMOS Ltd, All rights reserved
-#ifndef _reboot_h_
-#define _reboot_h_
-
-void device_reboot(void);
-
-#endif
--- TX-8600/src/user/could_serve.h
+++
@@ -1,9 +0,0 @@
-#ifndef  __COULD_SERVER_H_
-#define  __COULD_SERVER_H_
-
-void could_heart_send_timer();
-
-
-
-#endif 	//__COULD_SERVER_H_
-
